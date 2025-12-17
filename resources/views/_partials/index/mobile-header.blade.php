97a70581c57ab9c38d6df2fd1fb79046aab875ba
Sammy Joseph <sam.tab.paz@gmail.com>
2025-12-16 22:45:26 -0500

    responsive design (3)

--- resources/views/_partials/index/mobile-header.blade.php
+++ resources/views/_partials/index/mobile-header.blade.php
@@ -15,46 +15,8 @@
                 <!-- mobile menu start -->
                 <nav>
                     <ul class="mobile-menu">
-                        <li class="menu-item-has-children"><a href="index.html">Home</a>
-                            <ul class="dropdown">
-                                <li><a href="index.html">Home version 1 </a></li>
-                                <li><a href="index-2.html">Home version 2</a></li>
-                                <li><a href="index-3.html">Home version 3</a></li>
-                                <li><a href="index-4.html">Home version 4</a></li>
-                                <li><a href="index-5.html">Home version 5</a></li>
-                                <li><a href="index-6.html">Home version 6</a></li>
-                                <li><a href="index-7.html">Home version 7</a></li>
-                                <li><a href="index-8.html">Home version 8</a></li>
-                                <li><a href="index-9.html">Home version 9</a></li>
-                                <li><a href="index-10.html">Home version 10</a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-item-has-children "><a href="#">shop</a>
-                            <ul class="dropdown">
-                                <li class="menu-item-has-children"><a href="#">shop layout</a>
-                                    <ul class="dropdown">
-                                        <li><a href="shop.html">standard style</a></li>
-                                        <li><a href="shop-list.html">shop list style</a></li>
-                                        <li><a href="shop-fullwide.html">shop fullwide</a></li>
-                                        <li><a href="shop-no-sidebar.html">grid no sidebar</a></li>
-                                        <li><a href="shop-list-no-sidebar.html">list no sidebar</a></li>
-                                        <li><a href="shop-right-sidebar.html">shop right sidebar</a></li>
-                                        <li><a href="store-location.html">store location</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children"><a href="#">Products Layout</a>
-                                    <ul class="dropdown">
-                                        <li><a href="product-details.html">tab style 1</a></li>
-                                        <li><a href="product-details-2.html">tab style 2</a></li>
-                                        <li><a href="product-details-sticky.html">sticky style</a></li>
-                                        <li><a href="product-details-gallery.html">gallery style </a></li>
-                                        <li><a href="product-details-affiliate.html">affiliate style</a></li>
-                                        <li><a href="product-details-group.html">group style</a></li>
-                                        <li><a href="product-details-fixed-img.html">fixed image style </a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
+                        <li class="menu-item-has-children"><a href="{{ route('index') }}">Inicio</a></li>
+                        <li class="menu-item-has-children "><a href="{{ route('shop') }}">Tienda</a></li>
                         <li class="menu-item-has-children"><a href="#">Pages</a>
                             <ul class="dropdown">
                                 <li><a href="about-us.html">about us </a></li>
