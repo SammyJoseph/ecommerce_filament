03fd2ea7fc575bab502abaf7296765d03e61d7b8
SammyJoseph <sam.tab.paz@gmail.com>
2025-12-18 15:25:34 -0500

    responsive design (4)

--- resources/views/_partials/index/scripts.blade.php
+++ resources/views/_partials/index/scripts.blade.php
@@ -13,7 +13,7 @@
 <script src="{{ asset('assets/js/plugins/easyzoom.js') }}"></script>
 <script src="{{ asset('assets/js/plugins/scrollup.js') }}"></script>
 <script src="{{ asset('assets/js/plugins/ajax-mail.js') }}"></script>
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<script src="{{ asset('assets/js/main.js?v=0.02') }}"></script>
 
 <script>
     document.addEventListener('livewire:initialized', () => {
